e0bf1b3a0f7198d5ccb79b9f88bfba25b8f16fea
sabreen atalla ahmed <sabryntallh22@gmail.com>
2025-08-24 18:50:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using HospitalDB.Helpers;
+
+using HospitalDB.Models;
+using HospitalDB.Servies;
+using HospitalDB.Servies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace HospitalDB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Database
+            builder.Services.AddDbContext<HospitalDBContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("HospitalConnection")));
+
+            // JWT Configuration
+            builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
+
+            var jwtSettings = builder.Configuration.GetSection("JWT").Get<JWT>();
+            var key = Encoding.UTF8.GetBytes(jwtSettings.Key);
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+      .AddEntityFrameworkStores<HospitalDBContext>()
+      .AddDefaultTokenProviders();
+
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+            builder.Services.AddScoped<IAuthService, AuthService>();
+         
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication(); // 🟢 مهم يجي قبل UseAuthorization
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
